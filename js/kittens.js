0f913f2978f2cb6ade67228aab095ca277e01bd5
Eric MacKay <eric.mackay@gmail.com>
2017-04-06 23:09:17 +0000

    solved bugs and refactored

--- js/kittens.js
+++ js/kittens.js
@@ -26,12 +26,18 @@ var images = {};
 });
 
 
-
-
+class Entity {
+        
+    render(ctx) {
+        ctx.drawImage(this.sprite, this.x, this.y);
+    }
+}
 
 // This section is where you will be doing most of your coding
-class Enemy {
+class Enemy extends Entity {
     constructor(xPos) {
+        super();
+        
         this.x = xPos;
         this.y = -ENEMY_HEIGHT;
         this.sprite = images['enemy.png'];
@@ -43,14 +49,12 @@ class Enemy {
     update(timeDiff) {
         this.y = this.y + timeDiff * this.speed;
     }
-
-    render(ctx) {
-        ctx.drawImage(this.sprite, this.x, this.y);
-    }
 }
 
-class Player {
+class Player extends Entity{
     constructor() {
+        super();
+        
         this.x = 2 * PLAYER_WIDTH;
         this.y = GAME_HEIGHT - PLAYER_HEIGHT - 10;
         this.sprite = images['player.png'];
@@ -64,16 +68,9 @@ class Player {
         else if (direction === MOVE_RIGHT && this.x < GAME_WIDTH - PLAYER_WIDTH) {
             this.x = this.x + PLAYER_WIDTH;
         }
+        
     }
 
-    render(ctx) {
-        ctx.drawImage(this.sprite, this.x, this.y);
-    }
-}
-
-
-class Entity {
-    
 }
 
 
@@ -206,7 +203,7 @@ class Engine {
                 if (this.enemies[i]  &&
                 this.player.x === this.enemies[i].x
                 &&
-                this.enemies[i].y + ENEMY_HEIGHT - 40 > this.player.y
+                this.enemies[i].y + ENEMY_HEIGHT - 90 > this.player.y
                 )
                 {
                     return false;
